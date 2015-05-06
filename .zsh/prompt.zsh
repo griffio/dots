633512ebc189324f795e1721dc8c77023e9a5d9f
griffio <griffio@users.noreply.github.com>
2015-05-06 09:31:48 +0100

    bart

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,10 +1,10 @@
 autoload -U promptinit && promptinit
-prompt adam2
+prompt bart
 
 function zle-line-init zle-keymap-select {
-	RPS1="${${KEYMAP/vicmd/-NOR-}/(main|viins)/-INS-}"
+    RPS1="${${KEYMAP/vicmd/-NOR-}/(main|viins)/-INS-}"
     RPS2=$RPS1
-	zle reset-prompt
+    zle reset-prompt
 }
 
 zle -N zle-line-init
