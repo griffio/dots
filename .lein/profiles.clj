91160f3223ba920ae0e9cfa24a20071c0cffeaec
griffio <griffio@users.noreply.github.com>
2015-07-15 18:28:37 +0100

    lein profiles
    venantius/ultra
    color-scheme

---
+++ .lein/profiles.clj
@@ -0,0 +1,14 @@
+{:user {:plugins [[venantius/ultra "0.3.4"]]
+        :ultra {:color-scheme {:delimiter [:red]
+                               :tag [:red]
+                               :nil [:cyan]
+                               :boolean [:cyan]
+                               :number [:cyan]
+                               :string [:cyan]
+                               :character [:cyan]
+                               :keyword [:green]
+                               :symbol nil
+                               :function-symbol [:blue]
+                               :class-delimeter [:blue]
+                               :class-name nil
+                               :exception nil}}}}
