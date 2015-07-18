c6a942de9cebbdb93680dd949883ab5354b43f6e
griffio <griffio@users.noreply.github.com>
2015-07-18 12:52:06 +0100

    alias cdgh
    cd toplevel directory in git

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -9,3 +9,7 @@ alias lsd='ls -aFhlG'
 alias l='ls -al'
 alias ls='ls -GFh' # Colorize output, add file type indicator, and put sizes in human readable format
 alias ll='ls -GFhl' # ditto ls, in long listing format
+
+#git
+alias cdgh='git rev-parse --show-toplevel'
+
