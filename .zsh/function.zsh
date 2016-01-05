ae4344bd0b30b4854ac7f358d77096e139666df2
griffio <griffio@users.noreply.github.com>
2016-01-05 11:54:23 +0000

    list javahomes

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -1,4 +1,8 @@
 function javahome() {
-  export JAVA_HOME=`/usr/libexec/java_home -v $@`
+if [ $@ ]; then
+        export JAVA_HOME=`/usr/libexec/java_home -v $@`
+else         
+        /usr/libexec/java_home -V 
+fi
 }
 
