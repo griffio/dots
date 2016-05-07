b9ca6cc31b3e57eaf577bb93f9dce79a22b1d51f
griffio <griffio@users.noreply.github.com>
2016-05-07 09:32:27 +0100

    bind forward delete
    use od command to get terminal sequence

---
+++ binding.zsh
@@ -0,0 +1,9 @@
+bindkey -v        # vi mode
+bindkey "^A" beginning-of-line
+bindkey "^B" backward-char
+bindkey "^E" end-of-line                          
+bindkey "^F" forward-char
+bindkey "^K" kill-whole-line                      
+bindkey "^R" history-incremental-search-backward 
+bindkey "^T" transpose-chars
+bindkey "^[[3~" delete-char
