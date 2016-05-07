097b018b060c9435bbd3e5be75760d5ad5cbe222
griffio <griffio@users.noreply.github.com>
2016-05-07 19:13:43 +0100

    Update binding.zsh

--- .zsh/binding.zsh
+++ .zsh/binding.zsh
@@ -1,10 +1,9 @@
 bindkey -v        # vi mode
+bindkey "^A"      beginning-of-line                      
+bindkey "^B"      backward-char                      
+bindkey "^E"      end-of-line                         
+bindkey "^F"      forward-char                       
 bindkey "^K"      kill-whole-line                      
 bindkey "^R"      history-incremental-search-backward 
-bindkey "^A"      beginning-of-line                      
-bindkey "^E"      end-of-line                          
-bindkey "[B"      history-search-forward              
-bindkey "[A"      history-search-backward             
-bindkey "^D"      delete-char                         
-bindkey "^F"      vi-cmd-mode                       
-bindkey "^[[3~" delete-char
+bindkey "^T"      transpose-chars
+bindkey "^[[3~"   delete-char
