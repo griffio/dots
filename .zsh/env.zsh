bfa24fd3f92183314ea1f8a5f87f311035073dfb
griffio <griffio@users.noreply.github.com>
2016-07-15 04:03:07 +0100

    golang

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -3,7 +3,7 @@ export KEYTIMEOUT=1
 export VISUAL=vim
 export EDITOR=$VISUAL
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-export GOPATH=$HOME/.go
+export GOPATH=/usr/local/share/.go
 export GOROOT=/usr/local/opt/go/libexec
 if [ -x /usr/libexec/path_helper ]; then
     PATH=''
